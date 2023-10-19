7643e3aa084383ec4649bed159e6b4c1b807cff2
Pedro Veiga Ribeiro <pedroveiga.ribeiro@gmail.com>
2023-10-19 14:10:15 -0300

    feat: Slider creation

--- Assets/_Project/Scripts/UI/MainMenuUIController.cs
+++ Assets/_Project/Scripts/UI/MainMenuUIController.cs
@@ -4,28 +4,49 @@ using Mono.Cecil.Cil;
 using UnityEngine;
 using UnityEngine.UIElements;
 
-public class MainMenuEventHandler : MonoBehaviour
+public class MainMenuUIController : MonoBehaviour
 {
 	Button _playButton;
+	
 	Button _settingsButton;
+	VisualElement _settingsModal;
+	Button _settingsReturnButton;
+	
 	Button _achievementsButton;
+	VisualElement _achievementsModal;
+	Button _achievementsReturnButton;
 	Button _creditsButton;
-	Button[] _returnButton;
 	
 	private void Start()
+	{
+		GetReferences();
+		RegisterEvents();
+	}
+	
+	private void GetReferences()
 	{
 		VisualElement root = GetComponent<UIDocument>().rootVisualElement;
 		
 		_playButton = root.Q<Button>("btnPlay");
-		_playButton.RegisterCallback<ClickEvent>(OnPlayButtonClicked);
 		
 		_settingsButton = root.Q<Button>("btnSettings");
-		_settingsButton.RegisterCallback<ClickEvent>(OnSettingsButtonClicked);
+		_settingsModal = root.Q<VisualElement>("settingsModal");
+		_settingsReturnButton = root.Q<Button>("settingsReturnButton");
 		
 		_achievementsButton = root.Q<Button>("btnAchievements");
-		_achievementsButton.RegisterCallback<ClickEvent>(OnAchievementsButtonClicked);
+		_achievementsModal = root.Q<VisualElement>("achievementsModal");
+		_achievementsReturnButton = root.Q<Button>("achievementsReturnButton");
 		
 		_creditsButton = root.Q<Button>("btnCredits");
+	}
+	
+	private void RegisterEvents()
+	{
+		_playButton.RegisterCallback<ClickEvent>(OnPlayButtonClicked);
+		_settingsButton.RegisterCallback<ClickEvent>(OnSettingsButtonClicked);
+		_settingsReturnButton.RegisterCallback<ClickEvent>(OnSettingsButtonClicked);
+		_achievementsButton.RegisterCallback<ClickEvent>(OnAchievementsButtonClicked);
+		_achievementsReturnButton.RegisterCallback<ClickEvent>(OnAchievementsButtonClicked);
 		_creditsButton.RegisterCallback<ClickEvent>(OnCreditsButtonClicked);
 	}
 	
@@ -35,12 +56,14 @@ public class MainMenuEventHandler : MonoBehaviour
 	}
 	private void OnSettingsButtonClicked(ClickEvent e)
 	{
-		Debug.Log("Settings!");
+		_settingsModal.ToggleInClassList("modal-div--closed");
 	}
+	
 	private void OnAchievementsButtonClicked(ClickEvent e)
 	{
-		Debug.Log("Achievements!");
+		_achievementsModal.ToggleInClassList("modal-div--closed");
 	}
+	
 	private void OnCreditsButtonClicked(ClickEvent e)
 	{
 		Debug.Log("Credits!");
